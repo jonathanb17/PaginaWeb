da5aba0e50cf514cc0d3e88909ccb905dc5b007c
Joni <bustamantejonathanandres@gmail.com>
2020-01-01 22:00:18 -0300

    version 1

---
+++ js/index.js
@@ -0,0 +1,16 @@
+window.addEventListener('load', () => {
+
+    // Slider
+
+
+    $('.galeria').bxSlider({
+        mode: 'fade',
+        captions: true,
+        slideWidth: 600,
+        responsive: true,
+        pager: true
+    });
+
+
+
+});
